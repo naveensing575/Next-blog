45815cf6882b5b09ca229245ee4b563a67334c5e
naveen.singh <naveen.singh@itelasoft.com>
2024-03-20 12:59:18 +0530

    add auth as an route group

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function login() {
+  return (
+    <h3>login</h3>
+  )
+}
+
+export default login
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+  import React from 'react'
+  
+  function register() {
+    return (
+      <h2>register</h2>
+    )
+  }
+  
+  export default register

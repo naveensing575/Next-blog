119f26f1c2261e54f139fa07464f6de786ae5865
naveen.singh <naveen.singh@itelasoft.com>
2024-04-05 11:35:43 +0530

    handling middleware, cache and cookies

--- src/app/dashboard/user/api/route.ts
+++ src/app/dashboard/user/api/route.ts
@@ -1,3 +1,23 @@
-export async function GET() {
-  return new Response('Welcome to User Dashboard API!')
+import { type NextRequest } from "next/server";
+import { cookies, headers } from "next/headers";
+
+export async function GET(req: NextRequest) {
+  const requestHeaders = new Headers(req.headers);
+  const headersList = headers();
+
+  cookies().set("results", "20");
+  const theme = req.cookies.get("theme");
+
+  console.log(requestHeaders.get('Authorization'));
+  console.log(headersList.get('Authorization'));
+
+  console.log('theme: ', theme);
+  console.log(cookies().get('results'));
+  return new Response('<h1>Hello, Next.js!</h1>',{
+    headers: {
+      "Set-Cookie": "theme=light",
+      "Content-Type": "text/html",
+    },
+    status: 200,
+  });
 }
